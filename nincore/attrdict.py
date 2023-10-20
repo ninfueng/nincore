e71a4b7e330c57c4f4eef39b8d8151e2c3b900ff
ninfueng <ninnart.fuengfusin@yahoo.com>
2023-10-20 21:16:00 +0900

    major: fixes `AttrDict.__repr__`.

--- nincore/attrdict.py
+++ nincore/attrdict.py
@@ -33,56 +33,56 @@ class AttrDict(OrderedDict):
     """
 
     __slots__ = ()
-    # `OrderedDict.__getitem__` causes `mypy` warnings. Using `dict` instead.
+    # NOTE: `OrderedDict.__getitem__` causes `mypy` warnings. Using `dict` instead.
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
-    # Default, what to replace with unseen keys.
     factory_default = None
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        for key in self.keys():
-            if isinstance(self[key], (OrderedDict, dict)):
-                self[key] = AttrDict(self[key])
-
-            elif isinstance(self[key], list):
-                for idx, value in enumerate(self[key]):
-                    if isinstance(value, dict):
-                        self[key][idx] = AttrDict(self[key][idx])
+        for k, v in self.items():
+            if isinstance(v, (dict, OrderedDict, AttrDict)):
+                # Recursively convert to `AttrDict`.
+                self[k] = AttrDict(v)
+            elif isinstance(v, (tuple, list)):
+                for idx, value in enumerate(v):
+                    if isinstance(value, (dict, OrderedDict, AttrDict)):
+                        self[k][idx] = AttrDict(v[idx])
 
     def __missing__(self, key: str) -> None:
+        """Default is `None`, what to replace with unseen keys."""
         self[key] = self.factory_default
         return self.factory_default
 
     def to_json(self, json_dir: str, indent: int = 4) -> None:
-        assert isinstance(json_dir, str), f'Should be `str`, your type `{type(json_dir)}`.'
-        assert isinstance(indent, int), f'Should be `int`, your type `{type(indent)}`.'
+        assert isinstance(json_dir, str), f'Should be `str`, Your `{type(json_dir)}`.'
+        assert isinstance(indent, int), f'Should be `int`, Your `{type(indent)}`.'
         json_dir = os.path.expanduser(json_dir)
 
         self._cvt_array_list()
-        self._not_exist_makedirs(json_dir)
+        self._if_not_exist_makedirs(json_dir)
         with open(json_dir, 'w') as f:
             json.dump(self, f, indent=indent, default=lambda _: None)
 
     def to_yaml(self, yaml_dir: str) -> None:
-        assert isinstance(yaml_dir, str), f'Should be `str`, your type `{type(yaml_dir)}`.'
+        assert isinstance(yaml_dir, str), f'Should be `str`, Your `{type(yaml_dir)}`.'
         yaml_dir = os.path.expanduser(yaml_dir)
 
         self._cvt_array_list()
-        self._not_exist_makedirs(yaml_dir)
+        self._if_not_exist_makedirs(yaml_dir)
         with open(yaml_dir, 'w') as f:
             yaml.dump(self, f)
 
     def get_dict_repr(self, d: Dict[str, Any], indent: int) -> str:
         s = '{\n'
-        for k_, v_ in d.items():
+        for k, v in d.items():
             s += ' ' * indent
-            if isinstance(v_, (AttrDict, OrderedDict, dict)):
+            if isinstance(v, (dict, OrderedDict, AttrDict)):
                 indent += 2
-                s += self.get_dict_repr(v_, indent)
+                s += self.get_dict_repr(v, indent)
                 indent -= 2
             else:
-                s += f'{k_}: {v_},'
+                s += f'{k}: {v},'
             s += '\n'
         s += ' ' * (indent - 2) + '},'
         return s
@@ -92,35 +92,51 @@ class AttrDict(OrderedDict):
         s = 'AttrDict{\n'
         for k, v in self.items():
             s += ' ' * 2
-            if isinstance(v, (AttrDict, OrderedDict, dict)):
-                indent += 2
-                s += self.get_dict_repr(v, indent)
-                indent -= 2
+            if isinstance(v, (dict, OrderedDict, AttrDict)):
+                s += f'{k}: '
+                if len(v) == 0:
+                    s += '{},'
+                else:
+                    indent += 4
+                    s += self.get_dict_repr(v, indent)
+                    indent -= 4
             else:
                 s += f'{k}: {v},'
             s += '\n'
         s += '}'
         return s
 
-    def _not_exist_makedirs(self, dirname: str) -> None:
+    def _if_not_exist_makedirs(self, dirname: str) -> None:
         dirname = os.path.dirname(dirname)
+        if dirname == '' or dirname == '.':
+            return
         os.makedirs(dirname, exist_ok=True)
 
     def _cvt_array_list(self) -> None:
         """Converts `Tensor` and `np.ndarray` to `list` to save-able formats."""
-        for key, value in self.items():
-            if isinstance(value, np.ndarray):
-                self[key] = value.tolist()
+        for k, v in self.items():
+            if isinstance(v, np.ndarray):
+                self[k] = v.tolist()
+            elif isinstance(v, (dict, OrderedDict, AttrDict)):
+                if isinstance(v, AttrDict):
+                    self[k]._cvt_array_list()
+                else:
+                    self[k] = AttrDict(v)
+                    self[k]._cvt_array_list()
             if ENABLE_TORCH:
-                if isinstance(value, torch.Tensor):
-                    tmp = value.detach().cpu().numpy()
-                    self[key] = tmp.tolist()
+                if isinstance(v, torch.Tensor):
+                    self[k] = v.detach().cpu().numpy().tolist()
 
 
 if __name__ == '__main__':
     a = AttrDict(
         a=1,
         b=[1, 2],
-        c=AttrDict({'d': 3, 'e': AttrDict({'f': 4, 'h': {'k': 5, 'x': 6}}), 'g': 5}),
+        c=AttrDict({'d': 3, 'e': AttrDict({'f': 4, 'h': {'h': 5, 'j': 6}}), 'g': 5}),
+        k={},
+        l=[],
+        m=np.array([1, 2, 3]),
+        n=np.array([[1, 2, 3], [4, 5, 6]]),
+        o={'p': np.array([1, 2, 3]), 'q': np.array([[1, 2, 3], [4, 5, 6]])},
     )
     print(a)
