49f8947607585d569962a715e20554f137dfd80b
ninfueng <ninnart.fuengfusin@yahoo.com>
2024-04-11 10:05:37 +0900

    minor: changes name of version related functions from * -> *_ver

--- nincore/version.py
+++ nincore/version.py
@@ -1,24 +1,25 @@
 """Version related functions."""
+
 from types import ModuleType
 
 from packaging.version import Version, parse
 
 __all__ = [
-    'parse_module',
-    'is_newer_equal',
-    'is_newer',
-    'is_older_equal',
-    'is_older',
-    'is_equal',
+    'parse_module_ver',
+    'is_newer_equal_ver',
+    'is_newer_ver',
+    'is_older_equal_ver',
+    'is_older_ver',
+    'is_same_ver',
 ]
 
 
-def parse_module(module: ModuleType) -> Version:
+def parse_module_ver(module: ModuleType) -> Version:
     """Parse version from given module.
 
     Example:
     >>> import torch
-    >>> parse_module(torch)
+    >>> parse_module_ver(torch)
     1.12.1+cu113
     """
     version = getattr(module, '__version__')
@@ -26,49 +27,49 @@ def parse_module(module: ModuleType) -> Version:
     return version
 
 
-def is_newer(module: ModuleType, version: str) -> bool:
+def is_newer_ver(module: ModuleType, version: str) -> bool:
     """Return True if `module.__version__` is newer than `version`.
 
     Example:
     >>> import torch
-    >>> is_newer(torch, '0.0.0')
+    >>> is_newer_ver(torch, '0.0.0')
     True
     """
-    module_version = parse_module(module)
+    module_version = parse_module_ver(module)
     version = parse(version)
     return module_version > version
 
 
-def is_newer_equal(module: ModuleType, version: str) -> bool:
+def is_newer_equal_ver(module: ModuleType, version: str) -> bool:
     """Return True if `module.__version__` is newer or equal than `version`.
 
     Example:
     >>> import torch
-    >>> is_newer_equal(torch, '0.0.0')
+    >>> is_newer_equal_ver(torch, '0.0.0')
     True
     """
-    module_version = parse_module(module)
+    module_version = parse_module_ver(module)
     version = parse(version)
     return module_version >= version
 
 
-def is_older(module: ModuleType, version: str) -> bool:
+def is_older_ver(module: ModuleType, version: str) -> bool:
     """Return True if `module.__version__` is newer or equal than `version`."""
-    module_version = parse_module(module)
+    module_version = parse_module_ver(module)
     version = parse(version)
     return module_version < version
 
 
-def is_older_equal(module: ModuleType, version: str) -> bool:
+def is_older_equal_ver(module: ModuleType, version: str) -> bool:
     """Return True if `module.__version__` is newer than `version`."""
-    module_version = parse_module(module)
+    module_version = parse_module_ver(module)
     version = parse(version)
     return module_version <= version
 
 
-def is_equal(module: ModuleType, version: str) -> bool:
+def is_same_ver(module: ModuleType, version: str) -> bool:
     """Return True if `module.__version__` is newer than `version`."""
-    module_version = parse_module(module)
+    module_version = parse_module_ver(module)
     version = parse(version)
     return module_version == version
 
@@ -77,7 +78,7 @@ if __name__ == '__main__':
     import numpy as np
     import packaging
 
-    version = parse_module(np)
+    version = parse_module_ver(np)
     print(version)
     packaging.version.parse(np.__version__)
     print(version)
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,18 +1,18 @@
 import nincore
-from nincore.version import is_equal, is_newer, is_older, parse_module
+from nincore.version import is_newer_ver, is_older_ver, is_same_ver, parse_module_ver
 
 
 def test_parse_version() -> None:
-    parsed = parse_module(nincore)
+    parsed = parse_module_ver(nincore)
     assert nincore.__version__ == str(parsed)
 
 
 class TestVersion:
     def test_version_newer(self) -> None:
-        assert not is_newer(nincore, '9999.99.99')
+        assert not is_newer_ver(nincore, '9999.99.99')
 
     def test_version_older(self) -> None:
-        assert not is_older(nincore, '0.0.0')
+        assert not is_older_ver(nincore, '0.0.0')
 
     def test_version_equal(self) -> None:
-        assert is_equal(nincore, nincore.__version__)
+        assert is_same_ver(nincore, nincore.__version__)
